e6e1dae7f32c303ac1e8745d8b3e859dbe4673b5
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-09 11:11:39 +0530

    added above commits to server

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const path = require('path');
+const errorHandler = require('./middleware/error');
 
 
+//route files
+const auth = require('./controllers/auth.js');
+const posts = require('./controllers/posts.js');
 
+
+// config
 dotenv.config({path: './config/config.env'});
 
 
@@ -15,18 +21,30 @@ connectDB();
 
 const app = express();
 
+
+// json content
 app.use(express.json());
 
 
+
+// for rest api's
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
-
+// for static assets
 app.use(express.static(path.join(__dirname, 'public')))
 
+// routes
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/posts', posts);
+
+
+// Errors middleware
+app.use(errorHandler());
+// Port
 const PORT = process.env.PORT || 5000;
 
-
+// server
 const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
