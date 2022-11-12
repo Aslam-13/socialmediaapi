a4aa39a20acf1a158ebdd725d67a5bf8733b6722
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-12 10:32:18 +0530

    errors

--- middleware/error.js
+++ middleware/error.js
@@ -5,17 +5,17 @@ let error = {...err};
 error.message = err.message;
 
   console.log(err.stack.red);
-
+  // Mongodb bad objectId
   if(err.name === 'CastError'){
     const message = `Post not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
-
+  // Mongodb error, duplicate field entered already there in database
   if(err.code === 11000){
     const message = `Duplicate field value entered`;
     error = new ErrorResponse(message, 400);
   }
-
+  // Mongodb validation error, values not entered properly
   if(err.name === 'ValidationError'){
     const message = Object.values(err.errors).map(val => val.message);
     error = new ErrorResponse(message, 400);
